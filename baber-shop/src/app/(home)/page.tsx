5f22d5bf8ca417feb6854b187b627c8479fde079
Shystra <jlsp.2019@hotmail.com>
2024-02-12 15:19:18 -0300

    feat: add initial booking component

--- baber-shop/src/app/(home)/page.tsx
+++ baber-shop/src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import { format } from 'date-fns';
 import { ptBR } from "date-fns/locale";
 import Search from "../components/Search";
+import {BookingItem} from "../components/BookingItem";
 
 export default function Home() {
   return (
@@ -17,6 +18,10 @@ export default function Home() {
       </div>
       <div className="px-5 mt-6">
         <Search />
+      </div>
+      <div className="px-5 p-4">
+        <h2 className=" text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem />
       </div>
     </div>
   )
