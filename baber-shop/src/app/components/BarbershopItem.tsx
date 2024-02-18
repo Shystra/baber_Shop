43db4bac96afa4fecc2a49c052d9266fc23028de
Shystra <jlsp.2019@hotmail.com>
2024-02-18 11:28:09 -0300

    feat: add babershop item

--- baber-shop/src/app/components/BarbershopItem.tsx
+++ baber-shop/src/app/components/BarbershopItem.tsx
@@ -2,31 +2,42 @@ import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Barbershop } from "@prisma/client";
 import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import { StarIcon } from "lucide-react";
 
 interface IBarbershopItemProps {
     barbershop: Barbershop
 }
-export const BarbershopItem = ({ barbershop}: IBarbershopItemProps) => {
-    
+export const BarbershopItem = ({ barbershop }: IBarbershopItemProps) => {
+
     return (
         <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
             <CardContent className="px-1 py-0">
                 <div className="relative w-full h-[159px]">
-                <Image
-                alt={barbershop.name} 
-                src={barbershop.imageUrl} 
-                fill
-                style={{
-                    objectFit: "cover"
-                }}
-                className="rounded-2xl"
-                
-                />
+                    <div className="absolute top-2 left-2 z-50">
+                        <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
+                            <StarIcon size={12} className="fill-primary text-primary"/>
+                            <span>5,0</span>
+                        </Badge>
+                    </div>
+
+
+
+                    <Image
+                        alt={barbershop.name}
+                        src={barbershop.imageUrl}
+                        fill
+                        style={{
+                            objectFit: "cover"
+                        }}
+                        className="rounded-2xl"
+
+                    />
                 </div>
                 <div className="px-2 pb-3">
-                <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
-                <p className="text-sm text text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
-                <Button className=" w-full mt-3" variant="secondary">Reservar</Button>
+                    <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
+                    <p className="text-sm text text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
+                    <Button className=" w-full mt-3" variant="secondary">Reservar</Button>
                 </div>
             </CardContent>
         </Card>
